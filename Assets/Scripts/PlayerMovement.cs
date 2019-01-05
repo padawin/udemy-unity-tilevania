bdcb950cca9b9b322fa13c0a606cf278331dffc3
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-01-05 23:20:50 +0000

    Player movements reorganised to fix some bugs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,41 +15,66 @@ public class PlayerMovement : MonoBehaviour {
 		myAnimator = GetComponent<Animator>();
 	}
 
+	// Update is called once per frame
+	void Update() {
+		handleInput();
+		move();
+		jump();
+		fall();
+		updateOrientation();
+	}
+
 	float getDelta() {
 		return Input.GetAxis("Horizontal") * Time.deltaTime * speed;
 	}
 
-	void OnCollisionEnter2D(Collision2D collider) {
-		myAnimator.SetBool("Falling", false);
-	}
-
-	void jump() {
-		if (Mathf.Abs(rb.velocity.y) > minVerticalSpeedToJump) {
-			return;
-		}
-
-		rb.gravityScale = 1f;
-		if (Input.GetButtonDown("Jump")) {
-			rb.velocity = new Vector2(rb.velocity.x, jumpInitialVelocity);
-			myAnimator.SetBool("Jumping", true);
-		}
-	}
-
-	void fall() {
-		if (rb.velocity.y < 0 && !myAnimator.GetBool("Falling")) {
-			if (myAnimator.GetBool("Jumping")) {
-				rb.gravityScale = 2.5f;
+	void handleInput() {
+		if (!myAnimator.GetBool("Jumping") && !myAnimator.GetBool("Falling")) {
+			if (Input.GetButtonDown("Jump")) {
+				startJump();
 			}
-			myAnimator.SetBool("Jumping", false);
-			myAnimator.SetBool("Running", false);
-			myAnimator.SetBool("Falling", true);
 		}
+	}
+
+	void startJump() {
+		rb.velocity = new Vector2(rb.velocity.x, jumpInitialVelocity);
+		myAnimator.SetBool("Jumping", true);
 	}
 
 	void move() {
 		float delta = getDelta();
-		rb.velocity = new Vector2(getDelta(), rb.velocity.y);
+		rb.velocity = new Vector2(delta, rb.velocity.y);
 		myAnimator.SetBool("Running", delta != 0);
+		if (rb.velocity.y < 0) {
+			startFalling();
+		}
+	}
+
+	void jump() {
+		if (!myAnimator.GetBool("Jumping")) {
+			return;
+		}
+
+		if (rb.velocity.y < 0) {
+			rb.gravityScale = 2.5f;
+			myAnimator.SetBool("Jumping", false);
+			startFalling();
+		}
+	}
+
+	void startFalling() {
+		myAnimator.SetBool("Falling", true);
+	}
+
+	void fall() {
+		if (!myAnimator.GetBool("Falling")) {
+			return;
+		}
+
+		if (rb.velocity.y > -Mathf.Epsilon) {
+			rb.gravityScale = 1f;
+			myAnimator.SetBool("Falling", false);
+		}
 	}
 
 	void updateOrientation() {
@@ -62,13 +87,5 @@ public class PlayerMovement : MonoBehaviour {
 			transform.localScale.y,
 			transform.localScale.z
 		);
-	}
-
-	// Update is called once per frame
-	void Update() {
-		move();
-		jump();
-		fall();
-		updateOrientation();
 	}
 }
