c51e190fde5ff0a242b4ada0db346218e3840029
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-01-12 16:25:37 +0000

    First bonus added: Double jump

---
+++ Assets/Scripts/Bonuses/Bonus.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonus : MonoBehaviour {
+	[SerializeField] string bonusName;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.name == "Player") {
+			grant();
+			Destroy(gameObject);
+		}
+	}
+
+	void grant() {
+		FindObjectOfType<GameSession>().addPlayerBonus(bonusName);
+	}
+}
