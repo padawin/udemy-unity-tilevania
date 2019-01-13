62ee6dc038289e813424137e589e531bc50b8b2b
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-01-13 20:40:21 +0000

    Even non NPC can get hit

--- Assets/Scripts/DamageDealer.cs
+++ Assets/Scripts/DamageDealer.cs
@@ -6,11 +6,10 @@ public class DamageDealer : MonoBehaviour {
 	[SerializeField] int damages = 10;
 
 	void OnTriggerEnter2D(Collider2D collider) {
-		if (collider.name != "Player") {
-			return;
-		}
 		ActorHealth health= collider.GetComponent<ActorHealth>();
-		health.hit(this);
+		if (health != null) {
+			health.hit(this);
+		}
 	}
 
 	public int getDamages() {
