3f26e0866a661ff6bef25052f4bbb18ff4781601
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-01-13 14:04:57 +0000

    Enemies attack hitbox added

--- Assets/Scripts/DamageDealer.cs
+++ Assets/Scripts/DamageDealer.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageDealer : MonoBehaviour {
+	[SerializeField] Collider2D hitbox;
 	[SerializeField] int damages = 10;
 
 	public int getDamages() {
 		return damages;
+	}
+
+	public Collider2D getHitbox() {
+		return hitbox;
 	}
 }
