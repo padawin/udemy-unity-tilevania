b0c5ff7b67c8736e76d178ae6b86f62f737ff62e
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-01-14 21:21:32 +0000

    Hit response added to mushrooms

---
+++ Assets/Scripts/Actor/AI/GroundEnemyHitResponse.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundEnemyHitResponse : HitResponse {
+	[SerializeField] Vector2 hitProjection;
+	Rigidbody2D rb;
+	Actor actor;
+	Animator myAnimator;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+		actor = GetComponent<Actor>();
+		myAnimator = GetComponent<Animator>();
+	}
+
+	override public void run (GameObject source) {
+		float hitDirection = Mathf.Sign(
+			transform.position.x - source.transform.position.x
+		);
+		rb.velocity = new Vector2(hitDirection * hitProjection.x, hitProjection.y);
+		myAnimator.SetTrigger("BeingHit");
+		actor.block();
+	}
+}
