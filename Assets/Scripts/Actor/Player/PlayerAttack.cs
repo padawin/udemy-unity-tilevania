749313ed139c9a798eda5a7b00498bef04e62926
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-01-28 23:09:57 +0000

    Scafholding for the fireball when the player attacks

--- Assets/Scripts/Actor/Player/PlayerAttack.cs
+++ Assets/Scripts/Actor/Player/PlayerAttack.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
+	[SerializeField] float minTimeToLoad = 0.001f;
+
+	float timeButtonPressed;
+	bool isPreparingAttack = false;
+
 	Animator myAnimator;
 	Actor actor;
 	PlayerMovement playerMovement;
+	GameSession gameSession;
 
 	void Start() {
 		actor = GetComponent<Actor>();
 		playerMovement = GetComponent<PlayerMovement>();
 		myAnimator = GetComponent<Animator>();
+		gameSession = FindObjectOfType<GameSession>();
 	}
 
 	void Update() {
@@ -18,25 +25,38 @@ public class PlayerAttack : MonoBehaviour {
 			return;
 		}
 		handleInput();
+		// Attack button still pressed, Start loading a fireball
+		if (isPreparingAttack && pressedLong()) {
+		}
 	}
 
 	void handleInput() {
 		if (Input.GetButtonDown("Fire1")) {
+			timeButtonPressed = Time.realtimeSinceStartup;
 			prepareAttack();
 		}
 		else if (Input.GetButtonUp("Fire1")) {
+			if (isPreparingAttack && gameSession.playerHasBonus("Fireball")) {
+				Debug.Log("Throw fireball");
+			}
 			attack();
 		}
 	}
 
 	void prepareAttack() {
+		isPreparingAttack = true;
 		myAnimator.SetTrigger("PreparingAttack");
 	}
 
 	void attack() {
+		isPreparingAttack = false;
 		playerMovement.setDelta(0f);
 		actor.block();
 		myAnimator.SetTrigger("Attacking");
+	}
+
+	bool pressedLong() {
+		return Time.realtimeSinceStartup - timeButtonPressed > minTimeToLoad;
 	}
 
 	void stopAttack() {
