2923529bacecf0d5da95e440a654a13b35111a3b
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-02-03 19:27:59 +0000

    Level 2 - Unprogrammed bosses added

---
+++ Assets/Scripts/Actor/AI/BossAppearance.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAppearance : MonoBehaviour {
+	[SerializeField] GameObject[] enemies;
+
+	void OnTriggerEnter2D() {
+		foreach (GameObject enemy in enemies) {
+			enemy.SetActive(true);
+		}
+	}
+}
