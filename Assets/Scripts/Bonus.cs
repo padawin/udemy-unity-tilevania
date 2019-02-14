ded9dc02383cf3449463ec7e5d57e8dbf3480441
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-02-14 22:14:49 +0000

    SaveNotification is now Notification and used when picking bonuses

--- Assets/Scripts/Bonus.cs
+++ Assets/Scripts/Bonus.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Bonus : MonoBehaviour {
 	[SerializeField] string bonusName;
+	[SerializeField] Notification notification;
 
 	void OnTriggerEnter2D(Collider2D collider) {
 		if (collider.gameObject.name == "Player") {
 			grant();
+			notification.show(bonusName.ToUpper() + " BONUS GRANTED");
 			Destroy(gameObject);
 		}
 	}
