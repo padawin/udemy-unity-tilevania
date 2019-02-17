eeb26bf43dcbfc5e8bff42cc5f65b9be10afe5ae
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-02-17 18:36:42 +0000

    Scripts added to save objects based on a randomly attributed guid

---
+++ Assets/Scripts/Saveable.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum SaveableType {ACTIVATE, DEACTIVATE, DESTROY};
+
+public class Saveable : MonoBehaviour {
+	[SerializeField] SaveableType type;
+
+	GameSession gameSession;
+	string guid;
+
+	public void save() {
+		gameSession.saveSaveable(guid);
+	}
+
+	void Start() {
+		guid = GetComponent<Guid>().guid.ToString();
+		gameSession = FindObjectOfType<GameSession>();
+		if (gameSession.findSaveable(guid)) {
+			switch (type) {
+				case SaveableType.ACTIVATE:
+					gameObject.SetActive(true);
+					break;
+				case SaveableType.DEACTIVATE:
+					gameObject.SetActive(false);
+					break;
+				case SaveableType.DESTROY:
+					Destroy(gameObject);
+					break;
+			}
+		}
+	}
+}
