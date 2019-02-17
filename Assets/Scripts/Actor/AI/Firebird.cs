e347d0bb712c64ce5e6b05600b6ce2549e6d3970
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-02-17 13:53:41 +0000

    Make bird firing!

--- Assets/Scripts/Actor/AI/Firebird.cs
+++ Assets/Scripts/Actor/AI/Firebird.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum BirdState {STANDBY, GO_TO_START, ACTIVE};
+enum BirdState {STANDBY, GO_TO_START, FIRE, GO_TO_PLAYER};
 
 public class Firebird : Observer {
+	[SerializeField] FireProjectileSpawner fireProjectileSpawner;
 	[SerializeField] Vector2 startPosition;
 	[SerializeField] float speed;
+	[SerializeField] int direction = -1;
+
+	[SerializeField] float timeFiring = 7f;
 
 	Rigidbody2D rb;
 
@@ -22,12 +26,26 @@ public class Firebird : Observer {
 		state = BirdState.GO_TO_START;
 	}
 
+	IEnumerator startFiring() {
+		// @TODO serialize that
+		yield return new WaitForSeconds(1f);
+		state = BirdState.FIRE;
+	}
+
 	void Update() {
 		if (state == BirdState.GO_TO_START) {
 			goToStartPosition();
 		}
-		else if (state == BirdState.ACTIVE) {
+		else if (state == BirdState.STANDBY) {
 			rb.velocity = new Vector2(0f, 0f);
+		}
+		else if (state == BirdState.FIRE) {
+			if (!fireProjectileSpawner.isFiring()) {
+				StartCoroutine(
+					fireProjectileSpawner.fire(direction, timeFiring)
+				);
+				state = BirdState.STANDBY;
+			}
 		}
 	}
 
@@ -37,7 +55,8 @@ public class Firebird : Observer {
 		direction.Normalize();
 		rb.velocity = direction * speed;
 		if (reachedDestination()) {
-			state = BirdState.ACTIVE;
+			state = BirdState.STANDBY;
+			StartCoroutine(startFiring());
 		}
 	}
 
---
+++ Assets/Scripts/FireProjectileSpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireProjectileSpawner : MonoBehaviour {
+	[SerializeField] GameObject fireProjectilePrefab;
+	[SerializeField] float minTimeBetweenFireball = 0f;
+	[SerializeField] float maxTimeBetweenFireball = 1f;
+	[SerializeField] Vector2 minFireballDistance;
+	[SerializeField] Vector2 maxFireballDistance;
+
+	bool firing = false;
+
+	public bool isFiring() {
+		return firing;
+	}
+
+	public IEnumerator fire(int direction, float duration) {
+		StartCoroutine(stopFiring(duration));
+		firing = true;
+		while (firing) {
+			createFireball(direction);
+			yield return new WaitForSeconds(Random.Range(minTimeBetweenFireball, maxTimeBetweenFireball));
+		}
+	}
+
+	void createFireball(int direction) {
+		var fireball = Instantiate(
+			fireProjectilePrefab, transform.position, Quaternion.identity
+		);
+		fireball.GetComponent<Rigidbody2D>().velocity = new Vector2(
+			direction * Random.Range(minFireballDistance.x, maxFireballDistance.x),
+			Random.Range(minFireballDistance.y, maxFireballDistance.y)
+		);
+	}
+
+	IEnumerator stopFiring(float timeFiring) {
+		yield return new WaitForSeconds(timeFiring);
+		firing = false;
+	}
+}
