5c0becb824371c882e18507d573319fa36d0f664
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-02-27 21:47:31 +0000

    Script renamed to be usable by actors in general and not only the player

--- Assets/Scripts/Actor/HealthDisplay.cs
+++ Assets/Scripts/Actor/HealthDisplay.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealthDisplay : Observer {
+public class HealthDisplay : Observer {
 	[SerializeField] ActorHealth playerHealth;
 	[SerializeField] float widthMaxHealth;
 
