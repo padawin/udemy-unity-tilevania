f29efbd7c0d9f8e654c4908ae9d3a96bddbea6ac
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2019-03-24 18:22:38 +0000

    Fog in level 3 updated to be more seemless when going to an area to another

--- Assets/Scripts/Environment/FogCollider.cs
+++ Assets/Scripts/Environment/FogCollider.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FogCollider : MonoBehaviour {
+	int nbCollisions = 0;
+
 	void OnTriggerEnter2D(Collider2D collider) {
-		gameObject.GetComponent<Renderer>().enabled = false;
+		if (collider.name != "Player") {
+			return;
+		}
+		nbCollisions++;
+		if (nbCollisions > 0) {
+			gameObject.GetComponent<Renderer>().enabled = false;
+		}
 	}
 
 	void OnTriggerExit2D(Collider2D collider) {
-		gameObject.GetComponent<Renderer>().enabled = true;
+		if (collider.name != "Player") {
+			return;
+		}
+		nbCollisions--;
+		if (nbCollisions <= 0) {
+			gameObject.GetComponent<Renderer>().enabled = true;
+		}
 	}
 }
